edf95235692ace996f4f843836871d17cc45f4eb
Chaitanya Kharya <kharyachaitanya@gmail.com>
2024-01-17 17:12:57 +0530

    Goroutines

--- Assignment21_Goroutines/main.go
+++ Assignment21_Goroutines/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func numbers() {
+	for i := 0; i <= 5; i++ {
+		fmt.Printf("%d ", i)
+		time.Sleep(250 * time.Millisecond)
+
+	}
+}
+
+func alpha() {
+	for i := 'a'; i <= 'e'; i++ {
+		fmt.Printf("%c ", i)
+		time.Sleep(400 * time.Millisecond)
+
+	}
+}
+
+func hello() {
+	fmt.Println("Hello World Goroutines")
+}
+func main() {
+	go hello()
+	fmt.Println("Hello from main")
+	time.Sleep(1 * time.Second)
+
+	go numbers()
+	go alpha()
+	time.Sleep(3 * time.Second)
+	fmt.Println("Main terminated")
 }
